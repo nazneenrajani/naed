d711e10155ff8cdfb10c1d7159bf83d5f34999ef
Edaena <edaena@utexas.edu>
2013-04-25 01:17:03 -0500

    Removed the use of stop words.

--- word-relatedness.pl
+++ word-relatedness.pl
@@ -6,13 +6,23 @@
 
 use WordNet::QueryData;
 use WordNet::Similarity::lesk;
+use WordNet::SenseRelate::WordToSet;
 
 
 my $wn = WordNet::QueryData->new();
 my $lesk = WordNet::Similarity::lesk->new($wn);
-my %englishStopWords = (); # Dictionary of English stop words
+
+# To compute the sense of the words of a given phrase.   
+my %options = (wordnet => $wn,
+                      measure => 'WordNet::Similarity::lesk');#'WordNet::Similarity::lesk');
+
+my $wsd = WordNet::SenseRelate::WordToSet->new (%options);
+
 my %dictionary = (); # Dictionary of the words present in the texts
 my $totalWords = 0;
+my $totalContexts = 0;
+
+my @vectors = (); # Array that contains vectors of each sample context
 
 open INFILE, "edaena.txt" or die $!;
 my @lines = <INFILE>;
@@ -20,7 +30,7 @@ my @lines = <INFILE>;
 my $phrase = "";
 my $isLiterally;
 
-readStopWords(); # Populate englishStopWords dictionary.
+
 readDictionary(@lines); # Populate the dictionary of words present in the file.
 
 
@@ -41,32 +51,27 @@ foreach (@lines) {
         if ($sense eq "literally") { $isLiterally = 1; }
         else { $isLiterally = 0; }
     }
-    else
+    elsif (length($_) > 1) # ignore blank lines
     {
-        $tempPhrase = $phrase;
-        my @context = split(/ /, $_);
-        my @words;
-     
-        foreach (@context) {
-            my @word = split(/#/, $_);
-            #print $word[0] . " ";
-            if (exists $englishStopWords{$word[0]} )
-            {
- 	            #print $_ . "\n";
- 	         }
- 	        
-        } 
         
+        $tempPhrase = $phrase;
+        my @context = split(/ /, $_); # Words that are going to be compared against the phrase
+
         if ($isLiterally == 1) 
         {
-            $tempPhrase =~ s/\s+/_/g;
-            removeStopWords($phrase);
-            if (hasWordnetSense($tempPhrase)) { 
+            my @words = split(' ', $phrase);
+            my $tempPhrase = $phrase;
+            if ($words[0] eq "a") 
+             {
+                $tempPhrase = substr($phrase, 2);
+             }
+              $tempPhrase =~ s/\s+/_/g;
+             if (hasWordnetSense($tempPhrase)) { 
                 $phrase = $tempPhrase; # if the phrase has a sense use that sense.
             }
-            else { $phrase = removeStopWords($phrase); }
         }
-        else { $phrase = removeStopWords($phrase); }
+
+        getRelatednessVector(@context);
         
      } # else
 }
@@ -77,10 +82,6 @@ my $relatedness = $lesk->getRelatedness("car#n#1", "bus#n#2");
 
 die "$errorString\n" if($error);
 
-
-
-
-#print "$_\n" for keys %englishStopWords;
 
 #print "car (sense 1) <-> bus (sense 2) = $relatedness\n";
 
@@ -99,23 +100,6 @@ sub hasWordnetSense
    return $wn->querySense($p);
 }
 
-sub readStopWords
-{
-    #print "hello\n";
-    open FILE, "data/stopwords.english" or die $!;
-    my @lines = <FILE>;
-    foreach (@lines) 
-    {
-        $word = $_;
-        chomp($word);
-        $englishStopWords{$word} = 1;
-    }
-    close FILE;
-    if (exists $englishStopWords{"a"}) {
-        #print "**** IN exists\n";
-    }
-}
-
 sub readDictionary
 {
     @lines = @_;
@@ -123,8 +107,9 @@ sub readDictionary
         my $first_char = substr($_, 0, 1);
  	my $word;
     
-        if ($first_char ne '@' and $first_char ne '$')
+        if ($first_char ne '@' and $first_char ne '$' and (length($_) > 1) )
         {   
+            $totalContexts = $totalContexts + 1;
             $phrase = substr($_, 2);
             chomp($phrase);
             
@@ -136,7 +121,7 @@ sub readDictionary
                 my @word = split(/#/, $_);
                 my $word = lc($word[0]);
                 chomp($word);
-                if ($word ne "" and not(exists $englishStopWords{$word}) and not (exists $dictionary{$word}) )
+                if ($word ne "" and not (exists $dictionary{$word}) )
                 {
                     $dictionary{$word} = $totalWords;
                     $totalWords = $totalWords + 1;
@@ -145,29 +130,65 @@ sub readDictionary
          }
 
     }
-    #print @lines;
 }
 
-sub isStopWord
-{
-    return exists $englishStopWords{$_[0]};
-}
 
-sub removeStopWords
+
+
+sub getRelatednessVector
 {
-    @words = split(/ /, $_[0]);
-    $words = "";
+    my @phraseVectors = ();
+    my @arr = 0 x $totalWords;
+    my @phraseWords = split(/ /, $phrase);
+    my @context;
+    my $totalVectors = @phraseWords;
+    
+    
+    
+    #print "Getting relatedness vector:\n";
+    my @words = @_;
+
     foreach (@words)
     {
-        if (not isStopWord($_))
-        {
-            $words = $words . $_ . " ";
-        }
+        @word = split(/#/, $_);
+        push(@context, $word[0]);
     }
-    chomp($words);
-    return $words;
+    #print @context;
+    #@hello = @context;
+    #@context = ['cake', 'eat', 'tea'];
+    my $phraseSense = "";
+    foreach (@phraseWords) {
+        my $pSense ="";
+        my $result = $wsd->disambiguate (target => $_,
+                          context => [@context]);
+        my $max = 0;
+       foreach my $key (keys %$result) {
+            my $r = $result->{$key};
+            if ($r > $max) {
+                $max = $r;
+                $pSense = $key;
+            }
+           #print $key, ' : ', $result->{$key}, "\n";
+       }
+       $phraseSense = $phraseSense . $pSense . " ";
+    }
+    
+    chomp($phraseSense);
+    print($phraseSense);
+    
+  
+
+
+    #print @words;
+    #print "\n";
+    #print "phrase:  $p\n";
+    #print @arr;
 }
 
+sub getRelatedness
+{
+    
+}
 
 =pod
 print "$dictionary{$_} - $_\n" for keys %dictionary;
@@ -180,8 +201,17 @@ print "\n";
 
 
 #print "\n";
+@a = (1, 2, 3);
+@keys = keys %dictionary;
+$size = @keys;
 
-
+=pod
+print "Total contexts: $totalContexts\n";
+print "Total words: $totalWords\n";
+print "total words: $size\n";
+print $size;
+print "\n";
+=cut
 
 # if (exists $strings{$string}) {
 
